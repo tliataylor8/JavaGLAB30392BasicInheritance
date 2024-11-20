fe29dbec4eb63b8abd7ba76b39ffd3b0c1888fc4
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-11-20 15:07:57 -0500

    Updated lab

---
+++ src/Doctor.java
@@ -0,0 +1,16 @@
+public class Doctor {
+   private String DoctorName;
+   protected String Department;
+   
+//   public Doctor(string doctorName, String deartment) {
+//	   this.doctorName = doctorName;
+//	   this.department = department;
+//   }
+   
+  public void Doctor_Details() {
+       System.out.println("Doctor Details...");
+//       System.out.println("Name: " + doctorName);
+//       System.out.println("Department: " + department);
+   }
+}
+
